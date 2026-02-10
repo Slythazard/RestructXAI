58a3b5fc9f3935760a4f8280b8f775a7c0a841c9
Antrika Kashyap <146320403+antrika02@users.noreply.github.com>
2026-02-11 01:30:21 +0530

    Update API gateway to specify host and port

--- services/api-gateway/src/api_gateway/main.py
+++ services/api-gateway/src/api_gateway/main.py
@@ -10,4 +10,9 @@ def handler():
     return "This is the API-Gateway"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="127.0.0.1",
+        port=8000,   
+        debug=True
+    )
+
